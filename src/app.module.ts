8bf3db9b9326a2be82bd3e394a0bfe10f2880f16
ivan <ivanbelasich@gmail.com>
2025-05-10 21:13:34 -0300

    faet: add env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,26 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { BoardModule } from './tasks/task.module';
 import { TaskGateway } from './websockets/task.gateway';
 import { TaskController } from './tasks/controllers/task.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { config } from './config/config';
 
+console.log(process.env.MONGODB_URI);
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      process.env.MONGODB_URI ?? 'mongodb://localhost:27017/kanban',
-    ),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('mongodb.database.connectionString'),
+        dbName: configService.get<string>('mongodb.database.databaseName'),
+      }),
+      inject: [ConfigService],
+    }),
     BoardModule,
   ],
   controllers: [TaskController],
---
+++ src/config/config.ts
@@ -0,0 +1,15 @@
+import { ConfigProps } from '../interfaces/config.interfaces';
+
+export const config = (): ConfigProps => ({
+  port: parseInt(process.env.PORT || '4000', 10),
+  api: {
+    apiUrl: process.env.API_URL || 'http://localhost:4000',
+    httpTimeout: 1000,
+  },
+  mongodb: {
+    database: {
+      connectionString: process.env.MONGODB_URI || 'mongodb://localhost:27017/kanban',
+      databaseName: 'kanban',
+    },
+  },
+});
